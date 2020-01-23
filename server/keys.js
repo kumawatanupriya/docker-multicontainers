afbea9d1e6558b74ecd984d196deabbe986d0a6a
Anupriya Kumawat <anupriyakumawat95@gmail.com>
2020-01-23 17:49:39 +0530

    intial commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    redisHost: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT,
+    PgUser: process.env.PGUSER,
+    PgHost: process.env.PGHOST,
+    PgDatabase: process.env.PGDATABASE,
+    PgPassword: process.env.PGPASSWORD,
+    PgPort: process.env.PGPORT
+};
